319a90d0b98899ee0e908d8a392f14279879e22b
1Kill2Steal <imliterallyhim@duck.com>
2024-01-12 23:16:11 +0200

    made a single hash table implementation.

--- main.rs
+++ main.rs
@@ -1,76 +1,60 @@
 use std::io::{self, Write};
+use std::collections::HashMap;
 use rand::Rng; // time to handle randomized rps.
 
-fn handle_game(input: i32, computer_choice: i32) -> i32 {
-    let mut win_or_loss: i32 = 0; // 0 = loss, 1 = draw, 2 = win
-    match input {
-        1 => {
-            match computer_choice {
-                1 => {
-                    println!("Draw.");
-                    win_or_loss = 1;
-                }
-                2 => {
+fn handle_game(
+    input: u8,
+    computer_choice: u8,
+    score: &mut i32,
+    outcomes: &HashMap<(u8, u8), char>,
+) -> bool {
+
+    match outcomes.get(&(input, computer_choice)) {
+        Some(result) => {
+            match result {
+                &'d' => println!("Draw."),
+                &'l' => {
                     println!("Loss.");
+                    *score -= 1;
                 }
-                3 => {
-                    println!("Win!");
-                    win_or_loss = 2;
+                &'w' => {
+                    println!("Win.");
+                    *score += 1;
                 }
-                _ => {
-                    eprintln!("Undefined behavior.");
-                }
+                _ => unreachable!(),
             }
         }
-        2 => {
-            match computer_choice {
-                1 => {
-                    println!("Win!");
-                    win_or_loss = 2;
-                }
-                2 => {
-                    println!("Draw.");
-                    win_or_loss = 1;
-                }
-                3 => {
-                    println!("Loss.");
-                }
-                _ => {
-                    eprintln!("Undefined behavior.");
-                }
-            }
-        }
-        3 => {
-            match computer_choice {
-                1 => {
-                    println!("Loss.");
-                }
-                2 => {
-                    println!("Win!");
-                    win_or_loss = 2;
-                }
-                3 => {
-                    println!("Draw.");
-                    win_or_loss = 1;
-                }
-                _ => {
-                    eprintln!("Undefined behavior.");
-                }
-            }
-        }
-        _ => {
-            eprintln!("Undefined behavior.");
-        }
+        None => unreachable!(),
     }
 
+    println!("Current score: {}", *score);
 
-    win_or_loss
+    *score >= 0
+}
+
+fn generate_game_outcomes() -> HashMap<(u8, u8), char> {
+    let mut outcomes = HashMap::new();
+
+    // Define the outcomes, d = draw, w = win, l = loss
+    outcomes.insert((1, 1), 'd');
+    outcomes.insert((1, 2), 'l');
+    outcomes.insert((1, 3), 'w');
+    outcomes.insert((2, 1), 'w');
+    outcomes.insert((2, 2), 'd');
+    outcomes.insert((2, 3), 'l');
+    outcomes.insert((3, 1), 'l');
+    outcomes.insert((3, 2), 'w');
+    outcomes.insert((3, 3), 'd');
+
+    outcomes
 }
 
 fn main() {
     let mut rng = rand::thread_rng();
+    let outcomes = generate_game_outcomes();
     // value decreases on loss and increases on win.
     let mut score: i32 = 0;
+
     loop {
         let mut input = String::new();
         print!("Enter 1 (rock), 2 (paper) or 3 (scissors) exit = 0: ");
@@ -79,35 +63,31 @@ fn main() {
         io::stdin().read_line(&mut input)
             .expect("Failed to read line.");
 
-        match input.trim().parse::<i32>() {
-            Ok(number) => {
-                if number == 0 {
-                    break;
-                } else if number > 0 && number < 4 {
-                    let computer_choice: i32 = rng.gen_range(1..=3);
+        let Ok(number) = input.trim().parse::<u8>() else {
+            eprintln!("Error: invalid input");
+            continue;
+        };
+        
+        match number {
+            0 => {
+                break;
+            }
+            1..=3 => {
+                let computer_choice: u8 = rng.gen_range(1..=3);
 
-                    println!("Entered valid number. ({})", number);
-                    println!("Computer choice: {}", computer_choice);
+                println!("Entered valid number. ({})", number);
+                println!("Computer choice: {}", computer_choice);
 
-                    let result: i32 = handle_game(number, computer_choice);
-                    if result == 2 {
-                        score += 1;
-                    } else if result == 0 {
-                        score -= 1;
-                    }
-                    if score < 0 {
-                        println!("Skill issue, you got less than 0 points.");
-                        break;
-                    } else {
-                        println!("Current score: {}", score);
-                    }
+                if handle_game(number, computer_choice, &mut score, &outcomes) {
+                    continue;
                 } else {
-                    println!("Invalid number.");
+                    println!("Skill issue, you got less than 0 points.");
+                    break;
                 }
             }
-            Err(_) => {
-                eprintln!("Error: invalid input")
-            }
+            // This one isn't `unreachable!()`
+            // because the user could've had a typo.
+            _ => eprintln!("Enter a number from 0 to 3 included."),
         }
     }
 }
